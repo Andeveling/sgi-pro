2d8f33f229869bbf0fd7516f4bd37af92cb372b8
Andres Parra <andeveling@gmail.com>
2024-07-18 07:51:34 -0500

    fix

--- src/__tests__/app/HomePage.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import HomePage from '@/app/page';
-
-describe('HomePage', () => {
-  it('should render home page', () => {
-    render(<HomePage />);
-    expect(screen).toBeInTheDocument();
-  });
-});
